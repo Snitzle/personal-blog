af1cc4c89e80266132a5694773823e8d992b6f18
Craig Jones <craig@trsdesign.com>
2023-08-04 19:18:13 +0100

    initial site push

---
+++ themes/bad-writing-bad/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './**/*.{php,html,htm,js}',
+        './*.{php,html,htm,js}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+}
